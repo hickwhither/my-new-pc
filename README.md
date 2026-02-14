aa9940e0ef48a2d408bb5e648c7dd0594c754aa7
hickwhither <92677288+hickwhither@users.noreply.github.com>
2026-02-14 11:20:12 +0700

    yikes

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 i mean pressure cheat
+
+```
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hickwhither/my-new-pc/refs/heads/master/main_loader.lua"))()
+```
--- main.py
+++ main.py
@@ -3,14 +3,13 @@ import socketserver
 import os
 
 PORT = 8000
-
-# Lấy thư mục chứa file main.py
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-os.chdir(BASE_DIR)  # đảm bảo serve đúng folder chứa main.py
+os.chdir(BASE_DIR)
 
 Handler = http.server.SimpleHTTPRequestHandler
 
 with socketserver.TCPServer(("", PORT), Handler) as httpd:
     print(f"Serving at http://localhost:{PORT}")
     httpd.serve_forever()
+
